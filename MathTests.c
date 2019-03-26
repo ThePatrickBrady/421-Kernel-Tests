5558a7db1b44e06c2d8f6cecc4f0e20b20d992d2
Patrick Brady <pj.brady97@gmail.com>
2019-03-26 00:16:55 -0500

    Update MathTests.c

--- MathTests.c
+++ MathTests.c
@@ -3,32 +3,36 @@
 #include <comp421/yalnix.h>
 #include <comp421/hardware.h>
 
+/* Created by Patrick Brady and Simran Virk */
+
 int main(int argc, char **argv) {
     // printf("Init the fork tests\n");
     int x = Fork();
 
-    // if (x == 0) {
-    //     int y = 3/0;
-    //     if (1) {
-    //         TracePrintf(0, "Test 1 failed. y = %u\n", y);
-    //     }
-    // }
+    if (x == 0) {
+        int y = 3/0;
+        if (1) {
+            TracePrintf(0, "Test 1 failed. y = %u\n", y);
+        }
+    }
 
+    // The next two did not produce errors and should probably be removed.
+    
     // x = Fork();
     // if (x == 0) {
-    //     float y = 3.0/0;
+    //     float y = 3.0/0;  // Unable to get this to create an error
     //     TracePrintf(0, "The value of y is %u\n", y);
     //     if (1) {
     //         TracePrintf(0, "Test 2 failed.\n");
     //     }
     // }
 
-    x = Fork();
-    if (x == 0) {
-        int a = 2147483647;
-        int b = x * 2;
-        TracePrintf(0, "Test 3 Failed. b = %u a = %u\n", b, a);
-    }
-    return 0;
+//     x = Fork();
+//     if (x == 0) {
+//         int a = 2147483647;
+//         int b = x * 2;
+//         TracePrintf(0, "Test 3 Unable to get an overflow...  b = %u a = %u\n", b, a);
+//     }
+//     return 0;
 
 }
