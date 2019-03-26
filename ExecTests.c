1ffd91608d6cc16fb0b9799f8bc1c57b256ae7d3
Patrick Brady <pj.brady97@gmail.com>
2019-03-26 00:13:40 -0500

    Update ExecTests.c

--- ExecTests.c
+++ ExecTests.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <comp421/yalnix.h>
 
+/* Created by Patrick Brady and Simran Virk */
+
 int main(int argc, char **argv)
 {
     printf("Running Exec Test number %s.\n", argv[0]);
